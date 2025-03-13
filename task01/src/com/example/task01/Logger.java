76f5232459c0338cab85d28dc26da74126c097ad
Max Razin <razinmax02@gmail.com>
2025-03-13 22:54:41 +0500

    Task01 complete

---
+++ task01/src/com/example/task01/Logger.java
@@ -0,0 +1,100 @@
+package com.example.task01;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.time.LocalDateTime;
+
+public class Logger {
+    private static final ArrayList<Logger> loggers = new ArrayList<>();
+    private final String name;
+    private LogLevel level;
+
+    public enum LogLevel {
+        DEBUG,
+        INFO,
+        WARNING,
+        ERROR
+    }
+
+    public Logger(String name) {
+        this.name = name;
+        this.level = LogLevel.DEBUG;
+        loggers.add(this);
+    }
+
+    public static Logger getLogger(String name) {
+        for (Logger logger : loggers) {
+            if (logger.getName().equals(name)) {
+                return logger;
+            }
+        }
+        Logger newLogger = new Logger(name);
+        loggers.add(newLogger);
+        return newLogger;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public LogLevel getLevel(){
+        return level;
+    }
+
+    public void setLevel(LogLevel level) {
+        this.level = level;
+    }
+
+    public void debug(String message) {
+        log(LogLevel.DEBUG, message);
+    }
+
+    public void debug(String format, Object... args) {
+        log(LogLevel.DEBUG, format, args);
+    }
+
+    public void info(String message) {
+        log(LogLevel.INFO, message);
+    }
+
+    public void info(String format, Object... args) {
+        log(LogLevel.INFO, format, args);
+    }
+
+    public void warning(String message) {
+        log(LogLevel.WARNING, message);
+    }
+
+    public void warning(String format, Object... args) {
+        log(LogLevel.WARNING, format, args);
+    }
+
+    public void error(String message) {
+        log(LogLevel.ERROR, message);
+    }
+
+    public void error(String format, Object... args) {
+        log(LogLevel.ERROR, format, args);
+    }
+
+    public void log(LogLevel level, String message) {
+        if (level.ordinal() >= this.level.ordinal()) {
+            String formattedMessage = formatMessage(level, message);
+            System.out.println(formattedMessage);
+        }
+    }
+
+    public void log(LogLevel level, String format, Object... args) {
+        if (level.ordinal() >= this.level.ordinal()) {
+            String formattedMessage = formatMessage(level, String.format(format, args));
+            System.out.println(formattedMessage);
+        }
+    }
+
+    private String formatMessage(LogLevel level, String message) {
+        LocalDateTime now = LocalDateTime.now();
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        return String.format("[%s] %s %s %s - %s", level, date, time, name, message);
+    }
+}
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,6 +2,23 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
+        Logger logger1 = Logger.getLogger("TestLogger");
+        Logger logger2 = Logger.getLogger("TestLogger");
 
+        System.out.println("logger1 == logger2: " + (logger1 == logger2));
+
+        logger1.debug("Debug message");
+        logger1.info("Info message");
+        logger1.warning("Warning message");
+        logger1.error("Error message");
+
+        logger1.setLevel(Logger.LogLevel.WARNING);
+
+        logger1.debug("This debug message should not be printed");
+        logger1.info("This info message should not be printed");
+        logger1.warning("This warning message should be printed");
+        logger1.error("This error message should be printed");
+
+        logger1.error("Formatted error: %d + %d = %d", 2, 3, 5);
     }
 }
